d070111f9369aee8389372bfd89ea35d871aace4
hank <13061710755@sina.cn>
2017-08-23 13:47:15 +0800

    delete video fixed status,

--- public/js/components/video/spa-route.js
+++ public/js/components/video/spa-route.js
@@ -217,8 +217,10 @@ angular.module('spaModule')
             $scope.videoStatus = status;
             if ($scope.videoStatus.score && parseFloat($scope.videoStatus.score)) {
                 $scope.videoStatus.score = parseInt(parseFloat($scope.videoStatus.score) * 100);
-                document.getElementById('video-uploaded').style.opacity = '0';
-                $('#dimmer-video-grade').dimmer('show');
+                $timeout(function(){
+                    document.getElementById('video-uploaded').style.opacity = '0';
+                    $('#dimmer-video-grade').dimmer('show');
+                }, 1000);
             }
         }).catch(function (reason) {
             if (reason === 'processing') {
